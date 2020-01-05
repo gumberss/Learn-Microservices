47dfaf1d1457707d8d4fb8c57688368c266d4395
Gumbers <gustavomarinsuppi@gmail.com>
2020-01-05 16:35:10 -0300

    feat: Add trasnfer repository and its interface. Creation of GetTransferLogs methods on AplicationService and repository

---
+++ RabbitMq/MicroRabbit/MicroRabbit.Transfer.Domain/Interfaces/ITransferRepository.cs
@@ -0,0 +1,10 @@
+﻿using MicroRabbit.Transfer.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Transfer.Domain.Interfaces
+{
+    public interface ITransferRepository
+    {
+        IEnumerable<TransferLog> GetTransferLogs();
+    }
+}
