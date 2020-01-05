b1744d9c21e89cdee73ea11dcae364cda50f4af9
Gumbers <gustavomarinsuppi@gmail.com>
2020-01-05 16:41:37 -0300

    feat: add trasnfer repository and service to dependency injector

--- RabbitMq/MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ RabbitMq/MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -8,6 +8,11 @@ using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
+using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Application.Services;
+using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Data.Repository;
+using MicroRabbit.Transfer.Domain.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MicroRabbit.Infra.IoC
@@ -29,10 +34,13 @@ namespace MicroRabbit.Infra.IoC
 
             //Application Services
             services.AddTransient<IAccountService, AccountService>();
+            services.AddTransient<ITransferService, TransferService>();
 
             //Data
             services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<ITransferRepository, TransferRepository>();
             services.AddTransient<BankingDbContext>();
+            services.AddTransient<TransferDbContext>();
         }
     }
 }
