1416594919e1504d8ef15905bf03ce105f6bc664
Gumbers <gustavomarinsuppi@gmail.com>
2020-01-05 14:59:57 -0300

    feat: Publish event to RabbitMq from Domain Command Handler

--- RabbitMq/MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ RabbitMq/MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -31,6 +31,7 @@ namespace MicroRabbit.Banking.Api.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] AccountTransfer accountTransfer)
         {
+            _accountService.Transfer(accountTransfer);
 
             return Ok(accountTransfer);
         }
--- RabbitMq/MicroRabbit/MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
+++ RabbitMq/MicroRabbit/MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
@@ -1,9 +1,7 @@
 ﻿using MediatR;
 using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Events;
 using MicroRabbit.Domain.Core.Bus;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -20,6 +18,8 @@ namespace MicroRabbit.Banking.Domain.CommandHandlers
 
         public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
         {
+            _bus.Publish(new TransferCreatedEvent(request.From, request.To, request.Amount));
+
             return Task.FromResult(true);
         }
     }
