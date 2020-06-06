4c64a9a344b68a0c293632ec07188d51aebb186b
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-06 10:57:13 -0300

    feat: tell the nats streaming server that the listener is closed when the terminal is stoped

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,7 +10,12 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
 	console.log('Listener connected to NATS')
 
-	const options = stan.subscriptionOptions().setManualAckMode(true)
+  stan.on('close', () => {
+    console.log('NATS connection closed!')
+    process.exit()
+  })
+
+  const options = stan.subscriptionOptions().setManualAckMode(true)
 
 	const subscription = stan.subscribe(
 		'ticket:created',
@@ -28,3 +33,6 @@ stan.on('connect', () => {
     msg.ack()
 	})
 })
+
+process.on('SIGINT', () => stan.close())
+process.on('SIGTERM', () => stan.close())
