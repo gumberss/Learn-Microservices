b8e9c5c86f3351783e1ae962f315af709cf8f8d3
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-07 12:32:43 -0300

    feat: created interface for ticket created event data

--- nats-test/src/events/ticket-created-event.ts
+++ nats-test/src/events/ticket-created-event.ts
@@ -3,9 +3,11 @@ import { Subjects } from './subjects'
 export interface TicketCreatedEvent {
 	subject: Subjects.TicketCreated
 
-	data: {
-		id: string
-		title: string
-		price: number
-	}
+	data: TicketCreatedEventData
+}
+
+export interface TicketCreatedEventData {
+	id: string
+	title: string
+	price: number
 }
--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,14 +1,14 @@
 import { Listener } from './base-listener'
 import { Message } from 'node-nats-streaming'
-import { TicketCreatedEvent } from './ticket-created-event'
+import { TicketCreatedEvent, TicketCreatedEventData } from './ticket-created-event'
 import { Subjects } from './subjects'
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-	readonly subject = Subjects.TicketCreated
-
-	queueGroupName: string = 'paymenrs-service'
-
-	onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
+  readonly subject = Subjects.TicketCreated
+  
+  queueGroupName: string = 'paymenrs-service'
+  
+  onMessage(data: TicketCreatedEventData, msg: Message): void {
 		console.log('Event data ', data)
     
 		msg.ack()
