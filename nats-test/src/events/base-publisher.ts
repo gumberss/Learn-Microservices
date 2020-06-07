69a5e1ec7ce1c8de08bff35fd8c55f14c5d00260
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-07 12:45:33 -0300

    feat: adding publisher event base class

---
+++ nats-test/src/events/base-publisher.ts
@@ -0,0 +1,20 @@
+import { Stan } from 'node-nats-streaming'
+import { Subjects } from './subjects'
+
+interface Event {
+	subject: Subjects
+	data: any
+}
+
+export abstract class Publisher<T extends Event> {
+	abstract subject: T['subject']
+
+	constructor(private client: Stan) {
+  }
+
+  publish(data: T['data']){
+    this.client.publish(this.subject, data, () => {
+       console.log('Event published')
+    })
+  }
+}
