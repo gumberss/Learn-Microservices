fa31201bf7bed9b7b4c49c3cd5dba6899c1114f0
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-07 14:27:59 -0300

    feat: made publish metthod as async

--- nats-test/src/events/base-publisher.ts
+++ nats-test/src/events/base-publisher.ts
@@ -1,5 +1,6 @@
 import { Stan } from 'node-nats-streaming'
 import { Subjects } from './subjects'
+import { resolve } from 'dns'
 
 interface Event {
 	subject: Subjects
@@ -9,12 +10,17 @@ interface Event {
 export abstract class Publisher<T extends Event> {
 	abstract subject: T['subject']
 
-	constructor(private client: Stan) {
-  }
+	constructor(private client: Stan) {}
 
-  publish(data: T['data']){
-    this.client.publish(this.subject, JSON.stringify(data), () => {
-       console.log('Event published')
-    })
-  }
+	publish(data: T['data']) : Promise<void> {
+		return new Promise((resolve, reject) => {
+			this.client.publish(this.subject, JSON.stringify(data), err => {
+				if (err) {
+					return reject(err)
+        }
+        console.log('Event published to subject', this.subject)
+				resolve()
+			})
+		})
+	}
 }
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,14 +7,17 @@ const stan = nats.connect('ticketing', 'abc', {
 	url: 'http://localhost:4222',
 })
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
 	console.log('Publisher conected to NATS')
 
 	const publisher = new TicketCreatedPublisher(stan)
-
-	publisher.publish({
-		id: '321',
-		title: 'concert',
-		price: 20,
-	})
+	try {
+		await publisher.publish({
+			id: '321',
+			title: 'concert',
+			price: 20,
+		})
+	} catch (err) {
+		console.log(err)
+	}
 })
