60f957511265f0f019778da142e6fd94656584b0
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-11 23:10:51 -0300

    chore: creatting controller and product entity

---
+++ ECommerce/ECommerce/src/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public String Id { get; set; }
+        
+        public String Name { get; set; }
+
+        public String Category { get; set; }
+
+        public String Summary { get; set; }
+
+        public String ImageFile { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
